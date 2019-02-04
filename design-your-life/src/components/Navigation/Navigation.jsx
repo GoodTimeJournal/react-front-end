d3ef9b392e3bdfdb5d6aef217486b025227786af
Kingaa6 <Kingaa6@gmail.com>
2019-02-04 11:08:22 -0800

    Add login and registration components, and conditional rendering based on data state

--- design-your-life/src/components/Navigation/Navigation.jsx
+++ design-your-life/src/components/Navigation/Navigation.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import logo from '../../styles/nav-logo.png';
 
-const Navigation = () => {
+const Navigation = props => {
   return (
     <nav>
       <img className="logo" src={logo} alt="Good Time Journal" />
       <div className="primary-nav">
         <h5>Home</h5>
-        <h5>Account</h5>
+        <h5 onClick={props.logOut}>Log Out</h5>
       </div>
     </nav>
   );
