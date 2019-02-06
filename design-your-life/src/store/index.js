1ddbb1852641b56d11eaad314fad2c5deee59ba0
Eva <36014601+eeevvaaa@users.noreply.github.com>
2019-02-06 10:00:33 -0600

    Comment out dev tool making it user friendly

--- design-your-life/src/store/index.js
+++ design-your-life/src/store/index.js
@@ -9,7 +9,7 @@ const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(...middlewares),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
