5cefa073373aad568512e1371955ec98a523fe4c
Kingaa6 <Kingaa6@gmail.com>
2019-02-07 08:43:19 -0800

    Add reflection card structure and basic styling

--- design-your-life/src/components/Main/ReflectionCard.jsx
+++ design-your-life/src/components/Main/ReflectionCard.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 const ReflectionCard = props => {
   return (
     <div className="card">
-      <h1>journal entry: {props.journalEntry}</h1>
+      <div className="title-date">
+        <h4 className="title">Reflection</h4>
+        <h4 className="date">{props.timestamp}</h4>
+      </div>
+      <div className="reflection-summary">
+        <p>{props.journalEntry}</p>
+      </div>
     </div>
   );
 };
